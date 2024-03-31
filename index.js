3935226f39ef314d5cd93b531c472afb356251d6
ivan de santiago <idesantiagocontreras@gamil.com>
2024-03-31 15:14:29 -0600

    segundo commit

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import express from 'express';
 
 const app = express();
 
-const port = 3000;
-app.listen(port);
+const port = 3000 || process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send("Hello word");
 });
 
+app.listen(port);
 console.log(`Server on port ${port}`);
