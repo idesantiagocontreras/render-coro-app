e72a45426c26ada46ab5e57c1cf464ed36e2d412
ivan de santiago <idesantiagocontreras@gamil.com>
2024-03-31 16:20:49 -0600

    Base de datos postgres agregada

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from 'express';
+import {config} from 'dotenv';
+import pg from 'pg';
+
+config()
 
 const app = express();
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+})
 
 const port = 3000 || process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send("Hello word");
+});
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query("SELECT NOW()");
+    return res.json(result.rows[0]);
 });
 
 app.listen(port);
